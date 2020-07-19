fd0a66a44956c3916304f42464536403b24704d7
heisthepirate <mohitsingh1997@gmail.com>
2020-07-19 20:07:51 +0530

    Adding graphql api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL';
 
 function App() {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let isMounted  = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        repository(owner: "heIsThePirate" name: "wingy-music") {
+          name
+        }
+      }
+      `).then(response => {
+        // Avoid updating state if the component unmounted efore the fetch completes
+        if(!isMounted) {
+          return;
+        }
+        const data = response.data;
+        setName(data.repository.name);
+      }).catch(error => console.log(error));
+
+      return () => {
+        isMounted = false;
+      };
+  }, []);
+
+  // Render "Loading..." until the query completes
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name != null ? `Respository: ${name}` : "Loading..."}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
